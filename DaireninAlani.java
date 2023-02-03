c79b7544da0b95201c145b4f92b03e2160cd34ae
yakup <lvashy86@gmail.com>
2023-02-03 11:47:59 +0300

    03

---
+++ DaireninAlani.java
@@ -0,0 +1,5 @@
+public class DaireninAlani {
+    public static void main(String[] args){
+        System.out.println();
+    }
+}
---
+++ KDVTurtari.java
@@ -0,0 +1,5 @@
+public class KDVTurtari {
+    public static void main(String[] args){
+        System.out.println();
+    }
+}
